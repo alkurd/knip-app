dcb6071997883177ae13d5ff0297a873d1eb14f1
Jevayro Berenstein <99073048@mydavinci.nl>
2025-11-19 09:48:03 +0100

    knip app project files toevoegen naar andere map

---
+++ beginner-agenda-app/app/index.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+
+export default function Index() {
+  const [name, setName] = useState(""); // Voor de gebruikersnaam
+  const [password, setPassword] = useState(""); // Voor het wachtwoord
+  
+
+  // functions
+  const handleSubmit = () => {
+    Alert.alert("Invoer", `Gebruiker: ${name}\nWachtwoord: ${password}`);
+  };
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+      }}
+    >
+      <Text>Sign in </Text>
+      
+
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: "80%",
+          marginVertical: 10,
+          paddingHorizontal: 10,
+          borderRadius: 5,
+        }}
+        placeholder="Naam"
+        value={name}
+        onChangeText={setName}
+      />
+      
+     
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: "gray",
+          borderWidth: 1,
+          width: "80%",
+          marginVertical: 10,
+          paddingHorizontal: 10,
+          borderRadius: 5,
+        }}
+        placeholder="Wachtwoord"
+        secureTextEntry={true} // Verbergt de tekst voor wachtwoord
+        value={password}
+        onChangeText={setPassword}
+      />
+
+      <Button title="Login" onPress={handleSubmit} />
+    </View>
+  );
+}
---
+++ beginner-agenda-app/backend/server.js
@@ -0,0 +1,25 @@
+
+import express from "express";
+import dotenv from "dotenv";
+
+ dotenv.config();
+
+const app = express();// express is de server module zelf
+
+const PORT = process.env.PORT || 8081;
+
+// connectDB(process.env.DATABASE_URL)
+
+app.get("/",(req,res )=>{
+
+res.send("Het werkt")
+})
+
+
+console.log("my port","process.env.PORT");
+// functie die je feedback geeft welke port
+app.listen(8081,() =>{
+    
+    console.log("server is up and running on PORT:", PORT);
+
+});
